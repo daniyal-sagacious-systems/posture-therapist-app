b6adda3668615c0057037fcdaf406d0e9e80b1dd
daniyal-sagacious-systems <daniyal.khalid@sagacious.systems>
2023-05-23 15:30:42 +0500

    changes the db config

--- Backend/app/config/db.config.js
+++ Backend/app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "192.168.5.21",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "postureapp"
+  HOST: "sql12.freesqldatabase.com",
+  USER: "sql12620096",
+  PASSWORD: "ZMYrcpnGyh",
+  DB: "sql12620096"
 };
